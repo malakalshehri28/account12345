206f923a34af2380f953a88b5dc6e07e1ce2a517
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-01-21 22:32:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,27 @@
-const discord = new require("discord.js");
-const client = new discord.Client();
+const Discord = require("discord.js");
+const client = new Discord.Client();
 
 
 
-client.user.setGame(`FAISAL`,"http://twitch.tv//idk")
+client.on("ready", () => {
+console.log('By : faisal');
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'faisal',
+     details: `faisal`,
+     url: 'http://twitch.tv/idk',
+     state: `faisal`,
+    application_id: '377479790195769345',
+     assets: {
+        small_image: `377480550207717376`,
+        small_text: 'faisal',
+        large_image: `377480353259978752`,
+        large_text: `faisal` }
+
+  }
+    });
 });
 
 

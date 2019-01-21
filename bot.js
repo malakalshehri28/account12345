9b9adb4fcc2e9dc8c8b1ebfd593d69c24fed3cea
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-01-21 21:58:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,2 +1,36 @@
+const discord = new require("discord.js");
+const client = new discord.Client();
+
+
+
+const adminprefix = "!";;
+const devs = ['536967989904277535'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'playing')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`);
+} else 
+ if (message.content.startsWith(adminprefix + 'streaming')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير ستريم البوت إلى  ${argresult}**`);
+} else
+ if (message.content.startsWith(adminprefix + 'listening')) {
+  client.user.setActivity(argresult, {type: 'LISTENING'})
+    message.channel.sendMessage(`**تم تغيير ليسنق البوت إلى  ${argresult}**`);
+} else
+ if (message.content.startsWith(adminprefix + 'watching')) {
+  client.user.setActivity(argresult, {type: 'WATCHING'})
+    message.channel.sendMessage(`**تم تغيير واتشنق البوت إلى  ${argresult}**`);
+} 
+
+});
+
+
+
+
+
 
 client.login(process.env.BOT_TOKEN);

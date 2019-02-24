83979c519c3c510647a1f6a530ef00f651c7356c
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-24 13:29:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,22 +3,7 @@ const client = new Discord.Client();
 
 
 
-client.on("ready", () => {
-client.user.setPresence({
-  status: 'dnd',
-  game: { 
-     type: 0,
-     name: 'Sometimes my Friends Open My Heart.',
-     details: `مشغول لاحد يكلمني`,
-     url: 'http://twitch.tv/idk',
-     state: `bbm:-222444`,
-    
-  }
-    });
-});
-
-
-
+client.user.setGame(`Off`)
 
 
 client.login(process.env.BOT_TOKEN);

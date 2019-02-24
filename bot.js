5b586507baff147134e008a810ac6d87e9cfc891
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-02-24 13:34:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,9 +1,32 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+var prefix = !
 
 
+if (message.content.startsWith(PREFIX + 'sw')) {
+  KiNg66S.user.setActivity(argresult, {type: 'WATCHING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`**Watch Now: **${argresult}`)
+} 
 
-client.user.setGame("Off")
+ 
+  if (message.content.startsWith(PREFIX + 'sl')) {
+  KiNg66S.user.setActivity(argresult, {type: 'LISTENING'})
+     console.log('test' + argresult);
+    message.channel.sendMessage(`LISTENING Now: **${argresult}`)
+} 
+
+
+if (message.content.startsWith(PREFIX + 'ss')) {
+  KiNg66S.user.setGame(argresult, "https://www.twitch.tv/idk");
+     console.log('test' + argresult);
+    message.channel.sendMessage(`**Streaming: **${argresult}`)
+} 
+if (message.content.startsWith(PREFIX + 'sp')) {
+  KiNg66S.user.setGame(argresult);
+     console.log('test' + argresult);
+    message.channel.sendMessage(`Playing: **${argresult}`)
+} 
 
 
 client.login(process.env.BOT_TOKEN);

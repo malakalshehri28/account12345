efd9a93627d854f0d1d7f6f9ff87a51983d86cba
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-03-09 21:57:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,4 +3,15 @@ const client = new Discord.Client();
 
 
 
+client.on('guildMemberAdd', member => {
+  
+  const channel = member.guild.channels.find(ch => ch.name === 'pure');
+ 
+  if (!channel) return;
+
+  channel.send(`Welcome to the server ❤`);
+});
+
+
+
 client.login(process.env.BOT_TOKEN);

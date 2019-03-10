be82d8ad78cb48af5b468570081558a0d279fbcd
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-03-10 22:26:04 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 client.on('guildMemberAdd',async member => {
-  if(member.guild.id !== '554343696368140303') return;
+  if(member.guild.id !== '509836720112402444') return;
   setTimeout(function(){
   member.guild.channels.find(r => r.id === '551019141297995786').send('**Welcome To Server Pure:hearts:**');
 },1600);

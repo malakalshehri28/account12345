de1530ec68b400f670372790361a75e289728262
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-20 05:42:21 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,11 +8,8 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`FAISAL`);
-client.user.setstatus(`dnd`);
+client.user.setGame(`Sometimes I want to die`,"http://twitch.tv//idk")
 });
-
-
   
   
 client.login(process.env.BOT_TOKEN);

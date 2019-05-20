06e586393caa698fcd39aa215c1cc04728d6f7a4
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-20 04:59:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,8 +9,29 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Some friends are not your friends`,"http://twitch.tv//idk")
+   
+   
+
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'dnd',
+  game: { 
+     type: 0,
+     name: 'f9o',
+     details: `f9o`,
+     url: 'http://twitch.tv/Streammingg',
+     state: `f9o`,
+    application_id: '281376075802476544',
+     assets: {
+        small_image: `366835431037337600`,
+        small_text: 'f9o' ,
+        large_image: `370451271133429760`,
+        large_text: `f9o` }
+
+  }
+    });
 });
+
 
 
   

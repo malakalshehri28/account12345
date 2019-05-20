41f73eeb2982b4a8b2445ad67eccff2ce3eda004
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-20 05:31:08 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`FAISAL`,"http://twitch.tv//idk")
+client.user.setGame(`FAISAL`);
 });
 
 

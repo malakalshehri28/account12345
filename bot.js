20095a634546d4de5c604552d08d943134a770ee
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-20 05:04:01 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,36 +1,28 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const fs = require("fs")
-var prefix = "#";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : AJ`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : AJ ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-   
-   
+
+
 
 client.on("ready", () => {
 client.user.setPresence({
   status: 'dnd',
   game: { 
      type: 0,
-     name: 'f9o',
-     details: `f9o`,
+     name: 'فيصل :) ',
+     details: `فيصل`,
      url: 'http://twitch.tv/Streammingg',
-     state: `f9o`,
+     state: `فيصل`,
     application_id: '281376075802476544',
      assets: {
         small_image: `366835431037337600`,
-        small_text: 'f9o' ,
+        small_text: ' فيصل ' ,
         large_image: `370451271133429760`,
-        large_text: `f9o` }
+        large_text: `فيصل` }
 
   }
     });
 });
+
 
 
 

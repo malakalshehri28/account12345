f966bc77fb2c791fa2145250ad4b7f94035acfc6
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-27 23:26:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,31 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "#";
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : faisal`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : faisal ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`nothing`,"http://twitch.tv//idk")
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+client.on("ready", () => {
+client.user.setPresence({
+  status: 'online', //الحال
+  game: {
+     type: 0,
+     name: 'WEEK_END 🔥',    
+     details: `🎈خميس ومالي خلق ازعل🎈`,  
+     url: 'http://twitch.tv/Streammingg',
+     state: `انا فوق السحاب هناك شوفوني ☁`,
+    application_id: '420956479747784715', //ايدي البوت اللي رافع عليه الصور
+     assets: {
+        small_image: `421232881684316160`, //ايدي الصوره الصغيره
+        small_text: 'Hi' ,//الكلام اللي يضهر على الصوره الصغيره
+        large_image: `421233046612869120`, //ايدي الصوره الكبيره
+        large_text: `Hi` } //الكلام اللي يضهر على الصوره الكبيره
+ 
+  }
+    });
 });
-
-
-
-
 
 
   

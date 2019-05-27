0f2a1f48fceaf4f5cbc25a94e16e02658b77f7f4
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-27 23:34:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,32 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
- 
- 
- 
- 
- 
- 
- 
-client.on("ready", () => {
-client.user.setPresence({
-  status: 'online', //الحال
-  game: {
-     type: 0,
-     name: 'hi',    
-     details: `hi2`,  
-     url: 'http://twitch.tv/Streammingg',
-     state: `hi3`,
-    application_id: '420956479747784715', //ايدي البوت اللي رافع عليه الصور
-     assets: {
-        small_image: `421232881684316160`, //ايدي الصوره الصغيره
-        small_text: 'Hi' ,//الكلام اللي يضهر على الصوره الصغيره
-        large_image: `421233046612869120`, //ايدي الصوره الكبيره
-        large_text: `Hi` } //الكلام اللي يضهر على الصوره الكبيره
- 
-  }
-    });
+
+
+
+
+
+client.user.setActivity('test', { type: 'PLAYING' })
+client.user.setStatus('idle')
+
+  
 });
 
 
-  
+
+ 
 client.login(process.env.BOT_TOKEN);

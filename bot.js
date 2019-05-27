06761ba10f088050ba12eab2d738d527609843e1
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-27 05:00:03 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,14 +2,13 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "#";
 client.on('ready', () => {
-        client.user.setStatus('idle');
    console.log(`----------------`);
       console.log(`Desert Bot- Script By : faisal`);
         console.log(`----------------`);
       console.log(`ON ${client.guilds.size} Servers '     Script By : faisal ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame("-");
+client.user.setActivity('test', { type: 'PLAYING' })
 
 
   

d1d413119fd5380287064cb20232b9779adeb384
malakalshehri28 <46494268+malakalshehri28@users.noreply.github.com>
2019-05-27 05:07:14 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ client.on('ready', () => {
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
 client.user.setActivity('test', { type: 'PLAYING' })
-client.user.setstatus("idle");
+client.user.setStatus('idle')
 
   
 });
